4a660372a62e26bf0293e0e5bfd443dfbf3ff5f9
AdenilsonNegri <adenilsonegri@gmail.com>
2025-09-01 23:40:13 -0300

    Primeiro commit Página em desenvolvimento

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      
+      <p className="read-the-docs">
+        Página em desenvolvimento...
+      </p>
+    </>
+  )
+}
+
+export default App
